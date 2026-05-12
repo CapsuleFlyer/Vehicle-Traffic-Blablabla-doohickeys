c5ca87d4e7f2544d421e6ab318d1e1b387c919f0
CapsuleFlyer <shaheerawan28@gmail.com>
2026-05-12 15:31:49 +0500

    hu

--- include/vehicle.h
+++ include/vehicle.h
@@ -9,19 +9,16 @@
 #define VEHICLE_TYPE_LEN 32
 #define LOCATION_LEN 20
 
-/* Vehicle priority levels */
-#define PRIORITY_EMERGENCY 3  /* Ambulance, Firetruck */
-#define PRIORITY_MEDIUM 2     /* Bus */
-#define PRIORITY_NORMAL 1     /* Car, Bike, Tractor */
+#define PRIORITY_EMERGENCY 3 
+#define PRIORITY_MEDIUM 2    
+#define PRIORITY_NORMAL 1    
 
-/* Vehicle directions */
 typedef enum {
     STRAIGHT,
     LEFT_TURN,
     RIGHT_TURN
 } direction_t;
 
-/* Vehicle states */
 typedef enum {
     SPAWNED,
     WAITING_AT_ENTRANCE,
@@ -32,7 +29,6 @@ typedef enum {
     LEFT_INTERSECTION
 } vehicle_state_t;
 
-/* Vehicle struct */
 typedef struct {
     int id;
     char type[VEHICLE_TYPE_LEN];
@@ -41,16 +37,15 @@ typedef struct {
     int priority;
     time_t arrival_time;
     vehicle_state_t current_state;
-    int intersection_id;  /* 0 for F10, 1 for F11 */
+    int intersection_id;  
     pthread_t thread_id;
     int is_active;
 } vehicle_t;
 
-/* Function prototypes */
 void* vehicle_thread_func(void* arg);
 vehicle_t* create_vehicle(int id, const char* type, int priority, int intersection_id);
 void destroy_vehicle(vehicle_t* vehicle);
 const char* get_vehicle_type_color(const char* type);
 const char* get_state_string(vehicle_state_t state);
 
-#endif /* VEHICLE_H */
+#endif 
